13b2a6123c393088f8d2e94cf6b70b2b61c38b72
pchchv <ipchchv@gmail.com>
2023-02-20 10:26:06 +0300

    Implement a TokenParser

--- notecalc/src/token_parser.rs
+++ notecalc/src/token_parser.rs
@@ -8,6 +8,9 @@ use rust_decimal::prelude::*;
 
 pub const APPLY_UNIT_OP_PREC: usize = 5;
 
+/// 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989
+pub const DECIMAL_PI: Decimal = Decimal::from_parts(1102470953, 185874565, 1703060790, false, 28);
+
 #[allow(variant_size_differences)]
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum TokenType {
@@ -195,3 +198,92 @@ impl OperatorTokenType {
         }
     }
 }
+
+pub struct TokenParser {}
+
+impl TokenParser {
+    pub fn parse_line<'text_ptr>(
+        line: &[char],
+        variable_names: &Variables,
+        dst: &mut Vec<Token<'text_ptr>>,
+        units: &Units,
+        line_index: usize,
+        allocator: &'text_ptr Bump,
+        function_param_count: usize,
+        func_defs: &FunctionDefinitions<'text_ptr>,
+    ) {
+        tracy_span("parse_line", file!(), line!());
+        let mut index = 0;
+        let mut can_be_unit = None;
+        let mut can_be_unit_converter = false;
+        if line.starts_with(&['#']) {
+            dst.push(Token {
+                ptr: allocator.alloc_slice_fill_iter(line.iter().map(|it| *it)),
+                typ: TokenType::Header,
+                has_error: false,
+            });
+            return;
+        }
+        while index < line.len() {
+            let parse_result = TokenParser::try_extract_token(
+                &line[index..],
+                variable_names,
+                &dst,
+                units,
+                line_index,
+                can_be_unit,
+                can_be_unit_converter,
+                allocator,
+                function_param_count,
+                func_defs,
+            );
+
+            match &parse_result.typ {
+                TokenType::Header => {
+                    // the functions already returned in this case
+                    panic!();
+                }
+                TokenType::StringLiteral => {
+                    if parse_result.ptr[0].is_ascii_whitespace() {
+                        // keep can_be_unit as it was
+                    } else {
+                        can_be_unit = None;
+                        can_be_unit_converter = false;
+                    }
+                }
+                TokenType::NumberLiteral(..) | TokenType::NumberErr => {
+                    can_be_unit = Some(UnitTokenType::ApplyToPrevToken);
+                    can_be_unit_converter = false;
+                }
+                TokenType::Unit(..) => {
+                    can_be_unit = Some(UnitTokenType::StandInItself);
+                    can_be_unit_converter = true;
+                }
+                TokenType::Operator(typ) => {
+                    match typ {
+                        OperatorTokenType::ParenClose => {
+                            can_be_unit = Some(UnitTokenType::ApplyToPrevToken);
+                        }
+                        OperatorTokenType::BracketClose => {
+                            // keep can_be_unit as it was
+                        }
+                        OperatorTokenType::UnitConverter => {
+                            can_be_unit = Some(UnitTokenType::StandInItself);
+                            can_be_unit_converter = false;
+                        }
+                        OperatorTokenType::Div => can_be_unit = Some(UnitTokenType::StandInItself),
+                        _ => {
+                            can_be_unit = None;
+                            can_be_unit_converter = false;
+                        }
+                    }
+                }
+                TokenType::Variable { .. } | TokenType::LineReference { .. } => {
+                    can_be_unit = Some(UnitTokenType::ApplyToPrevToken);
+                    can_be_unit_converter = true;
+                }
+            }
+            index += parse_result.ptr.len();
+            dst.push(parse_result);
+        }
+    }
