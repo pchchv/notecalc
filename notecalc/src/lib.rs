200cc54cc1ad5c3933a2b55f67391ddb30bcd3ed
pchchv <ipchchv@gmail.com>
2023-02-22 10:55:09 +0300

    Implement a helper mod

--- notecalc/src/lib.rs
+++ notecalc/src/lib.rs
@@ -353,6 +353,514 @@ pub fn try_extract_function_def<'b>(
     return Some(fd);
 }
 
+pub mod helper {
+    use crate::calc::CalcResultType;
+    pub use crate::{MAX_LINE_COUNT, *};
+    use std::ops::{Index, IndexMut};
+
+    #[derive(Debug)]
+    pub struct EditorObjects(Vec<Vec<EditorObject>>);
+
+    impl EditorObjects {
+        pub fn new() -> EditorObjects {
+            EditorObjects(
+                std::iter::repeat(Vec::with_capacity(8))
+                    .take(MAX_LINE_COUNT)
+                    .collect::<Vec<_>>(),
+            )
+        }
+
+        pub fn clear(&mut self) {
+            self.0.clear();
+        }
+
+        pub fn push(&mut self, d: Vec<EditorObject>) {
+            self.0.push(d);
+        }
+    }
+
+    impl Index<ContentIndex> for EditorObjects {
+        type Output = Vec<EditorObject>;
+
+        fn index(&self, index: ContentIndex) -> &Self::Output {
+            &self.0[index.0]
+        }
+    }
+
+    impl IndexMut<ContentIndex> for EditorObjects {
+        fn index_mut(&mut self, index: ContentIndex) -> &mut Self::Output {
+            &mut self.0[index.0]
+        }
+    }
+
+    pub struct Results([LineResult; MAX_LINE_COUNT]);
+
+    impl Results {
+        pub fn new() -> Results {
+            Results([Ok(None); MAX_LINE_COUNT])
+        }
+        pub fn as_slice(&self) -> &[LineResult] {
+            &self.0[..]
+        }
+
+        pub fn as_mut_slice(&mut self) -> &mut [LineResult] {
+            &mut self.0[..]
+        }
+    }
+
+    impl Index<ContentIndex> for Results {
+        type Output = LineResult;
+
+        fn index(&self, index: ContentIndex) -> &Self::Output {
+            &self.0[index.0]
+        }
+    }
+
+    impl IndexMut<ContentIndex> for Results {
+        fn index_mut(&mut self, index: ContentIndex) -> &mut Self::Output {
+            &mut self.0[index.0]
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct AppTokens<'a>([Option<Tokens<'a>>; MAX_LINE_COUNT]);
+
+    impl<'a> AppTokens<'a> {
+        pub fn new() -> AppTokens<'a> {
+            AppTokens([None; MAX_LINE_COUNT])
+        }
+
+        pub fn iter(&self) -> std::slice::Iter<Option<Tokens<'a>>> {
+            self.0.iter()
+        }
+    }
+
+    impl<'a> Index<ContentIndex> for AppTokens<'a> {
+        type Output = Option<Tokens<'a>>;
+
+        fn index(&self, index: ContentIndex) -> &Self::Output {
+            &self.0[index.0]
+        }
+    }
+
+    impl<'a> IndexMut<ContentIndex> for AppTokens<'a> {
+        fn index_mut(&mut self, index: ContentIndex) -> &mut Self::Output {
+            &mut self.0[index.0]
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct BitFlag256 {
+        pub bitset: [u128; 2],
+    }
+
+    impl BitFlag256 {
+        pub fn empty() -> BitFlag256 {
+            BitFlag256 { bitset: [0; 2] }
+        }
+
+        fn get_index(row_index: usize) -> (usize, usize) {
+            let array_index = (row_index & (!127) > 0) as usize;
+            let index_inside_u128 = row_index & 127;
+            return (array_index, index_inside_u128);
+        }
+
+        pub fn set(&mut self, row_index: usize) {
+            let (array_index, index_inside_u128) = BitFlag256::get_index(row_index);
+            self.bitset[array_index] |= 1u128 << index_inside_u128;
+        }
+
+        pub fn single_row(row_index: usize) -> BitFlag256 {
+            let (array_index, index_inside_u128) = BitFlag256::get_index(row_index);
+            let mut bitset = [0; 2];
+            bitset[array_index] = 1u128 << index_inside_u128;
+            BitFlag256 { bitset }
+        }
+
+        #[inline]
+        pub fn clear(&mut self) {
+            self.bitset[0] = 0;
+            self.bitset[1] = 0;
+        }
+
+        pub fn all_rows_starting_at(row_index: usize) -> BitFlag256 {
+            if row_index >= MAX_LINE_COUNT {
+                return BitFlag256::empty();
+            }
+            let mut bitset = [0; 2];
+
+            let (array_index, index_inside_u128) = BitFlag256::get_index(row_index);
+            let s = 1u128 << index_inside_u128;
+            let right_to_s_bits = s - 1;
+            let left_to_s_and_s_bits = !right_to_s_bits;
+            bitset[array_index] = left_to_s_and_s_bits;
+            // the other int is either fully 1s (if the array_index is 0) or 0s (if array_index is 1)
+            let other_index = array_index ^ 1;
+            bitset[other_index] = std::u128::MAX * other_index as u128;
+
+            BitFlag256 { bitset }
+        }
+        pub fn multiple(indices: &[usize]) -> BitFlag256 {
+            let mut b = [0; 2];
+            for i in indices {
+                let (array_index, index_inside_u128) = BitFlag256::get_index(*i);
+                b[array_index] |= 1 << index_inside_u128;
+            }
+            let bitset = b;
+
+            BitFlag256 { bitset }
+        }
+
+        pub fn range_incl(from: usize, to: usize) -> BitFlag256 {
+            debug_assert!(to >= from);
+            if from >= MAX_LINE_COUNT {
+                return BitFlag256::empty();
+            } else if to >= MAX_LINE_COUNT {
+                return BitFlag256::range_incl(from, MAX_LINE_COUNT - 1);
+            }
+            fn set_range_u128(from: usize, to: usize) -> u128 {
+                let top = 1 << to;
+                let right_to_top_bits = top - 1;
+                let bottom = 1 << from;
+                let right_to_bottom_bits = bottom - 1;
+                return (right_to_top_bits ^ right_to_bottom_bits) | top;
+            }
+            let mut b = BitFlag256::empty();
+            if from < 128 {
+                b.bitset[0] = set_range_u128(from, to.min(127));
+            }
+            if to >= 128 {
+                b.bitset[1] = set_range_u128(((from as isize) - 128).max(0) as usize, to - 128);
+            }
+
+            return b;
+        }
+
+        #[inline]
+        pub fn merge(&mut self, other: BitFlag256) {
+            self.bitset[0] |= other.bitset[0];
+            self.bitset[1] |= other.bitset[1];
+        }
+
+        #[inline]
+        pub fn need(&self, line_index: ContentIndex) -> bool {
+            let (array_index, index_inside_u128) = BitFlag256::get_index(line_index.0);
+            ((1 << index_inside_u128) & self.bitset[array_index]) != 0
+        }
+
+        #[inline]
+        pub fn is_true(&self, line_index: usize) -> bool {
+            return self.need(content_y(line_index));
+        }
+
+        #[inline]
+        pub fn is_false(&self, line_index: usize) -> bool {
+            return !self.is_true(line_index);
+        }
+
+        #[inline]
+        pub fn is_non_zero(&self) -> bool {
+            (self.bitset[0] | self.bitset[1]) != 0
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct GlobalRenderData {
+        pub client_height: usize,
+        pub client_width: usize,
+        pub scroll_y: usize,
+        pub result_gutter_x: usize,
+        pub left_gutter_width: usize,
+        pub longest_visible_result_len: usize,
+        pub longest_visible_editor_line_len: usize,
+        pub current_editor_width: usize,
+        pub current_result_panel_width: usize,
+        editor_y_to_render_y: [Option<CanvasY>; MAX_LINE_COUNT],
+        editor_y_to_rendered_height: [usize; MAX_LINE_COUNT],
+        pub theme_index: usize,
+    }
+
+    impl GlobalRenderData {
+        pub fn new(
+            client_width: usize,
+            client_height: usize,
+            result_gutter_x: usize,
+            left_gutter_width: usize,
+            right_gutter_width: usize,
+        ) -> GlobalRenderData {
+            let min_req_width =
+                MIN_RESULT_PANEL_WIDTH + RIGHT_GUTTER_WIDTH + LEFT_GUTTER_MIN_WIDTH + 4;
+            if client_width < min_req_width {
+                panic!(
+                    "client width is too small, it must be at least {} but it is {}",
+                    min_req_width, client_width
+                );
+            }
+            let mut r = GlobalRenderData {
+                scroll_y: 0,
+                longest_visible_result_len: 0,
+                longest_visible_editor_line_len: 0,
+                result_gutter_x,
+                left_gutter_width,
+                current_editor_width: 0,
+                current_result_panel_width: 0,
+                editor_y_to_render_y: [None; MAX_LINE_COUNT],
+                editor_y_to_rendered_height: [0; MAX_LINE_COUNT],
+                client_height: client_height.min(MAX_CLIENT_HEIGHT),
+                client_width,
+                theme_index: 0,
+            };
+            r.current_editor_width = (result_gutter_x - left_gutter_width) - 1;
+            r.current_result_panel_width = client_width - result_gutter_x - right_gutter_width;
+            // so tests without calling "paste" work
+            r.editor_y_to_rendered_height[0] = 1;
+            r
+        }
+
+        pub fn set_result_gutter_x(&mut self, x: usize) {
+            self.result_gutter_x = x;
+            // - 1 so that the last visible character in the editor is '…' if the content is to long
+            self.current_editor_width = (x - self.left_gutter_width) - 1;
+            self.current_result_panel_width = self.client_width - x - RIGHT_GUTTER_WIDTH;
+        }
+
+        pub fn set_left_gutter_width(&mut self, new_width: usize) {
+            self.left_gutter_width = new_width;
+            // - 1 so that the last visible character in the editor is '…' if the content is to long
+            self.current_editor_width = (self.result_gutter_x - new_width) - 1;
+        }
+
+        pub fn calc_bottom_y(&self, content_len: usize) -> CanvasY {
+            let bottom_i = content_y(content_len - 1);
+            return if let Some(y) = self.get_render_y(bottom_i) {
+                y.add(self.get_rendered_height(bottom_i))
+            } else {
+                canvas_y(self.client_height as isize)
+            };
+        }
+
+        pub fn clear_editor_y_to_render_y(&mut self) {
+            for e in self.editor_y_to_render_y.iter_mut() {
+                *e = None;
+            }
+        }
+
+        pub fn clear(&mut self) {
+            for e in self.editor_y_to_render_y.iter_mut() {
+                *e = None;
+            }
+            for e in self.editor_y_to_rendered_height.iter_mut() {
+                *e = 0;
+            }
+            self.scroll_y = 0;
+        }
+
+        pub fn is_visible(&self, y: ContentIndex) -> bool {
+            let top = match self.get_render_y(content_y(self.scroll_y)) {
+                Some(y) => y.as_isize(),
+                None => {
+                    return false;
+                }
+            };
+            return if let Some(y) = self.get_render_y(y) {
+                let y = y.as_isize();
+                y >= top && y < (top + self.client_height as isize)
+            } else {
+                false
+            };
+        }
+
+        pub fn get_render_y(&self, y: ContentIndex) -> Option<CanvasY> {
+            self.editor_y_to_render_y[y.0]
+        }
+
+        pub fn set_render_y(&mut self, y: ContentIndex, newy: Option<CanvasY>) {
+            self.editor_y_to_render_y[y.0] = newy;
+        }
+
+        pub fn editor_y_to_render_y(&self) -> &[Option<CanvasY>] {
+            &self.editor_y_to_render_y
+        }
+
+        pub fn get_rendered_height(&self, y: ContentIndex) -> usize {
+            self.editor_y_to_rendered_height[y.0]
+        }
+
+        pub fn set_rendered_height(&mut self, y: ContentIndex, h: usize) {
+            self.editor_y_to_rendered_height[y.0] = h;
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct PerLineRenderData {
+        pub editor_x: usize,
+        pub editor_y: ContentIndex,
+        pub render_x: usize,
+        pub render_y: CanvasY,
+        // contains the y position for each editor line
+        pub rendered_row_height: usize,
+        pub vert_align_offset: usize,
+        pub cursor_render_x_offset: isize,
+        // for rendering line number
+        pub line_num_digit_0: u8,
+        pub line_num_digit_1: u8,
+        pub line_num_digit_2: u8,
+    }
+
+    impl PerLineRenderData {
+        pub fn new() -> PerLineRenderData {
+            let r = PerLineRenderData {
+                editor_x: 0,
+                editor_y: content_y(0),
+                render_x: 0,
+                render_y: canvas_y(0),
+                rendered_row_height: 0,
+                vert_align_offset: 0,
+                cursor_render_x_offset: 0,
+                line_num_digit_0: 0,
+                line_num_digit_1: 0,
+                line_num_digit_2: 0,
+            };
+            r
+        }
+
+        pub fn inc_editor_y(&mut self) {
+            self.editor_y.0 += 1;
+        }
+
+        pub fn new_line_started(&mut self) {
+            self.editor_x = 0;
+            self.render_x = 0;
+            self.cursor_render_x_offset = 0;
+        }
+
+        pub fn line_render_ended(&mut self, row_height: usize) {
+            self.render_y.0 += row_height as isize;
+            self.editor_y.0 += 1;
+        }
+
+        pub fn set_fix_row_height(&mut self, height: usize) {
+            self.rendered_row_height = height;
+            self.vert_align_offset = 0;
+        }
+
+        pub fn calc_rendered_row_height(
+            result: &LineResult,
+            tokens: &[Token],
+            vars: &Variables,
+            active_mat_edit_height: Option<usize>,
+        ) -> usize {
+            let mut max_height = active_mat_edit_height.unwrap_or(1);
+            // determine max height based on result's height
+            let result_row_height = if let Ok(result) = result {
+                if let Some(result) = result {
+                    let result_row_height = match &result.typ {
+                        CalcResultType::Matrix(mat) => mat.render_height(),
+                        _ => max_height,
+                    };
+                    result_row_height
+                } else {
+                    max_height
+                }
+            } else {
+                max_height
+            };
+
+            // determine max height based on tokens' height
+            for token in tokens {
+                let token_height = match token.typ {
+                    TokenType::Operator(OperatorTokenType::Matrix {
+                        row_count,
+                        col_count: _,
+                    }) => MatrixData::calc_render_height(row_count),
+                    TokenType::LineReference { var_index } => {
+                        let var = &vars[var_index];
+                        match &var {
+                            Some(Variable {
+                                value:
+                                    Ok(CalcResult {
+                                        typ: CalcResultType::Matrix(mat),
+                                        ..
+                                    }),
+                                ..
+                            }) => mat.render_height(),
+                            _ => 1,
+                        }
+                    }
+                    _ => 1,
+                };
+                if token_height > max_height {
+                    max_height = token_height;
+                }
+            }
+            return max_height.max(result_row_height);
+        }
+
+        pub fn token_render_done(&mut self, editor_len: usize, render_len: usize, x_offset: isize) {
+            self.render_x += render_len;
+            self.editor_x += editor_len;
+            self.cursor_render_x_offset += x_offset;
+        }
+    }
+
+    #[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+    pub struct ContentIndex(usize);
+
+    #[inline]
+    pub fn content_y(y: usize) -> ContentIndex {
+        ContentIndex(y)
+    }
+
+    impl ContentIndex {
+        #[inline]
+        pub fn new(n: usize) -> ContentIndex {
+            ContentIndex(n)
+        }
+
+        #[inline]
+        pub fn as_usize(self) -> usize {
+            self.0
+        }
+
+        pub fn add(&self, n: usize) -> ContentIndex {
+            ContentIndex(self.0 + n)
+        }
+
+        pub fn sub(&self, n: usize) -> ContentIndex {
+            ContentIndex(self.0 - n)
+        }
+    }
+
+    #[derive(Clone, Copy, Eq, PartialEq, Debug, Ord, PartialOrd)]
+    pub struct CanvasY(isize);
+
+    pub fn canvas_y(y: isize) -> CanvasY {
+        CanvasY(y)
+    }
+
+    impl CanvasY {
+        pub fn new(n: isize) -> CanvasY {
+            CanvasY(n)
+        }
+
+        pub fn as_usize(self) -> usize {
+            self.0 as usize
+        }
+
+        pub fn as_isize(self) -> isize {
+            self.0
+        }
+
+        pub fn add(&self, n: usize) -> CanvasY {
+            CanvasY(self.0 + n as isize)
+        }
+
+        pub fn sub(&self, n: usize) -> CanvasY {
+            CanvasY(self.0 - n as isize)
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Variable {
     pub name: Box<[char]>,
