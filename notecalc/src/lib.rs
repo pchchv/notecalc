f11d6cf6eade95b2aaed5b5002f035c0b68abad3
pchchv <ipchchv@gmail.com>
2023-02-22 10:22:08 +0300

    Implement a FunctionDef and Theme

--- notecalc/src/lib.rs
+++ notecalc/src/lib.rs
@@ -1,1 +1,146 @@
+pub const MAX_FUNCTION_PARAM_COUNT: usize = 6;
 
+#[allow(dead_code)]
+pub struct Theme {
+    pub bg: u32,
+    pub func_bg: u32,
+    pub result_bg_color: u32,
+    pub selection_color: u32,
+    pub sum_bg_color: u32,
+    pub sum_text_color: u32,
+    pub reference_pulse_start: u32,
+    pub reference_pulse_end: u32,
+    pub number: u32,
+    pub number_error: u32,
+    pub operator: u32,
+    pub unit: u32,
+    pub variable: u32,
+    pub result_text: u32,
+    pub header: u32,
+    pub text: u32,
+    pub cursor: u32,
+    pub matrix_edit_active_bg: u32,
+    pub matrix_edit_active_text: u32,
+    pub matrix_edit_inactive_text: u32,
+    pub result_gutter_bg: u32,
+    pub left_gutter_bg: u32,
+    pub line_num_active: u32,
+    pub line_num_simple: u32,
+    pub scrollbar_hovered: u32,
+    pub scrollbar_normal: u32,
+    pub line_ref_bg: u32,
+    pub line_ref_text: u32,
+    pub line_ref_selector: u32,
+    pub referenced_matrix_text: u32,
+    pub change_result_pulse_start: u32,
+    pub change_result_pulse_end: u32,
+    pub current_line_bg: u32,
+    pub parenthesis: u32,
+}
+
+#[allow(dead_code)]
+impl Theme {
+    const DRACULA_BG: u32 = 0x282a36_FF;
+    const DRACULA_CURRENT_LINE: u32 = 0x44475a_FF;
+    const DRACULA_FG: u32 = 0xf8f8f2_FF;
+    const DRACULA_CYAN: u32 = 0x8be9fd_FF;
+    const DRACULA_COMMENT: u32 = 0x6272a4_FF;
+    const DRACULA_GREEN: u32 = 0x50fa7b_FF;
+    const DRACULA_ORANGE: u32 = 0xffb86c_FF;
+    const DRACULA_PINK: u32 = 0xff79c6_FF;
+    const DRACULA_PURPLE: u32 = 0xbd93f9_FF;
+    const DRACULA_RED: u32 = 0xff5555_FF;
+    const DRACULA_YELLOW: u32 = 0xf1fa8c_FF;
+}
+
+pub const THEMES: [Theme; 2] = [
+    // LIGHT
+    Theme {
+        bg: 0xFFFFFF_FF,
+        func_bg: 0xEFEFEF_FF,
+        result_bg_color: 0xF2F2F2_FF,
+        result_gutter_bg: 0xD2D2D2_FF,
+        selection_color: 0xA6D2FF_FF,
+        sum_bg_color: 0x008a0d_FF,
+        sum_text_color: 0x000000_FF,
+        reference_pulse_start: 0x00FF7F_33,
+        reference_pulse_end: 0x00FF7F_00,
+        number: 0x8963c4_FF,
+        number_error: 0xde353d_FF,
+        operator: 0x3a88e8_FF,
+        unit: 0x048395_FF,
+        variable: 0xc26406_FF,
+        result_text: 0x000000_FF,
+        header: 0x000000_FF,
+        text: 0x8393c7_FF,
+        cursor: 0x000000_FF,
+        matrix_edit_active_bg: 0xBBBBBB_55,
+        matrix_edit_active_text: 0x000000_FF,
+        matrix_edit_inactive_text: 0x000000_FF,
+        left_gutter_bg: 0xF2F2F2_FF,
+        line_num_active: 0x000000_FF,
+        line_num_simple: 0xADADAD_FF,
+        scrollbar_hovered: 0xFFBBBB_FF,
+        scrollbar_normal: 0xFFCCCC_FF,
+        line_ref_text: 0x000000_FF,
+        line_ref_bg: 0xDCE2F7_FF,
+        line_ref_selector: 0xDCE2F7_FF,
+        referenced_matrix_text: 0x000000_FF,
+        change_result_pulse_start: 0xFF88FF_AA,
+        change_result_pulse_end: 0xFFFFFF_55,
+        current_line_bg: 0xFFFFCC_FF,
+        parenthesis: 0x565869_FF,
+    },
+    // DARK
+    Theme {
+        bg: Theme::DRACULA_BG,
+        func_bg: 0x292B37_FF,
+        result_bg_color: 0x3c3f41_FF,
+        result_gutter_bg: 0x313335_FF,
+        selection_color: 0x214283_FF,
+        sum_bg_color: Theme::DRACULA_GREEN,
+        sum_text_color: 0x000000_FF,
+        reference_pulse_start: 0x00FF7F_33,
+        reference_pulse_end: 0x00FF7F_00,
+        number: Theme::DRACULA_PURPLE,
+        number_error: Theme::DRACULA_RED,
+        operator: 0x5bb0ff_FF, // Theme::DRACULA_YELLOW,
+        unit: Theme::DRACULA_CYAN,
+        variable: Theme::DRACULA_ORANGE,
+        result_text: Theme::DRACULA_FG,
+        header: Theme::DRACULA_FG,
+        text: Theme::DRACULA_COMMENT + 0x444444_00,
+        cursor: Theme::DRACULA_FG,
+        matrix_edit_active_bg: 0xBBBBBB_55,
+        matrix_edit_active_text: 0x000000_FF,
+        matrix_edit_inactive_text: 0x000000_FF,
+        left_gutter_bg: 0x3c3f41_FF,
+        line_num_active: 0xa3a2a0_FF,
+        line_num_simple: 0x4e6164_FF,
+        scrollbar_hovered: 0x4f4f4f_FF,
+        scrollbar_normal: 0x4b4b4b_FF,
+        line_ref_bg: 0x7C92A7_FF,
+        line_ref_text: 0x000000_FF,
+        line_ref_selector: Theme::DRACULA_BG + 0x333300_00,
+        referenced_matrix_text: 0x000000_FF,
+        change_result_pulse_start: 0xFF88FF_AA,
+        change_result_pulse_end: Theme::DRACULA_BG - 0xFF,
+        current_line_bg: Theme::DRACULA_CURRENT_LINE,
+        parenthesis: Theme::DRACULA_PINK,
+    },
+];
+
+#[allow(non_snake_case)]
+#[inline]
+pub fn NOT(a: bool) -> bool {
+    !a
+}
+
+#[derive(Debug)]
+pub struct FunctionDef<'a> {
+    pub func_name: &'a [char],
+    pub param_names: [&'a [char]; MAX_FUNCTION_PARAM_COUNT],
+    pub param_count: usize,
+    pub first_row_index: ContentIndex,
+    pub last_row_index: ContentIndex,
+}
