ebf263bab277698e7e3a566f98019d1cdff8f6f7
pchchv <ipchchv@gmail.com>
2023-02-25 13:57:00 +0300

    Implement a get_command_buffer_ptr and js_log

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use wasm_bindgen::prelude::*;
+
+mod utils;
+
+static mut RENDER_COMMAND_BUFFER: [u8; RENDER_COMMAND_BUFFER_SIZE] =
+    [0; RENDER_COMMAND_BUFFER_SIZE];
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn js_log(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[wasm_bindgen]
+pub fn get_command_buffer_ptr() -> *const u8 {
+    unsafe {
+        return RENDER_COMMAND_BUFFER.as_ptr();
     }
 }
